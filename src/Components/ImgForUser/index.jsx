ea03cc7d9d60425c4b075078bcc3944c9831c5b6
fazliddin-web79 <yakubjonovfazliddin777@gmail.com>
2022-08-12 20:31:37 -0700

    All project is done

--- src/Components/ImgForUser/index.jsx
+++ src/Components/ImgForUser/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Container } from "./style";
+import avatar from "../../Assets/img/avatar.jpg";
 
 const ImgForUser = ({ img, onClick }) => {
   return (
     <Container onClick={() => onClick(img)}>
-      <img src={img} alt="user avatar" />
+      <img src={img ? img : avatar} alt="user avatar" />
     </Container>
   );
 };
